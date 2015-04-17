1186032fe5895bda8f10da939c1fcf46a315197d
Antonio Murgia <tmnd91@gmail.com>
2015-04-18 01:09:02 +0200

    added buffer

--- src/main/scala/it/unibo/ing/smacs/monit/MainServlet.scala
+++ src/main/scala/it/unibo/ing/smacs/monit/MainServlet.scala
@@ -7,7 +7,8 @@ package it.unibo.ing.smacs.monit
  */
 
 import java.io.{IOException, InputStreamReader, BufferedReader}
-import it.unibo.ing.smacs.monit.gatherer.DataGatherer
+import java.util.Date
+import it.unibo.ing.smacs.monit.gatherer.{RecentBuffer, DataGatherer}
 import it.unibo.ing.smacs.monit.model.MonitInfo
 import it.unibo.ing.utils.BufferedLineIterator
 
@@ -27,11 +28,27 @@ class MainServlet extends MonitrestfulinterfaceStack {
     contentType = "json"
   }
 
-  get("/") {
-    import DefaultJsonProtocol._
-    DataGatherer.getData() match{
-      case Some(data: Seq[MonitInfo]) => data.toJson.compactPrint
-      case _ => JsObject("error"->JsString("cannot retrieve monit data"))
+  get("/:time") {
+    val tick = params("time")
+    val realDate = new Date(tick.toLong)
+    if (RecentBuffer isLatest realDate){
+      val newData = DataGatherer.getData()
+      import DefaultJsonProtocol._
+      newData match{
+        case Some(data: Seq[MonitInfo]) => {
+          RecentBuffer(realDate) = data
+          data.toJson.compactPrint
+        }
+        case _ => JsObject("error"->JsString("cannot retrieve monit data")).compactPrint
+      }
+    }
+    else{
+      import DefaultJsonProtocol._
+      if (RecentBuffer contains realDate){
+        RecentBuffer(realDate).toJson.compactPrint
+      }
+      else
+        JsObject("error"->JsString("cannot retrieve monit data")).compactPrint
     }
   }
 }
